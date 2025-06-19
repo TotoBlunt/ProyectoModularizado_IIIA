a4bb00d2a3dd86aab033132374c3c1312efee42f
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2025-06-18 19:06:40 -0500

    creado modelos e importando archivos pkl

---
+++ app.py
@@ -0,0 +1,68 @@
+import joblib
+import streamlit as st
+import pandas as pd
+from xgboost import XGBRegressor
+from utils.predicciones import predict_all
+
+# Configuración de la aplicación
+st.title('Predicción de Parámetros Avícolas')
+st.subheader('Predicciones de Mortalidad, Consumo, ICA y Peso Promedio Final')
+st.subheader('Ingrese los datos para realizar las predicciones')
+
+# Mapeos
+SEXO_MAP = {'Macho': 1, 'Hembra': 0}
+AREA_MAP = {
+    'Calidad': 1, 'I. Respiratoria': 2, 'S. esquelético': 3,
+    'I. Intestinal': 4, 'Coccidia': 5, 'C. tóxico': 6,
+    'C. metabólico': 7, 'S. Inmunitario': 8
+}
+
+# Entrada de datos
+nombre_user = st.text_input('Nombre del usuario')
+cargo_user = st.text_input('Cargo del usuario')
+if not nombre_user or not cargo_user:
+    st.warning('Por favor, ingrese su nombre y cargo para continuar.')
+col1, col2 = st.columns(2)
+with col1:
+    areaAn = st.selectbox('Área de la granja', list(AREA_MAP.keys()))
+    sexo = st.selectbox('Sexo de los pollos', list(SEXO_MAP.keys()))
+with col2:
+    edadHTs = st.selectbox('Edad al sacrificio (días)', [14, 21, 28, 35])
+    edadventa = st.number_input('Edad de venta (días)', min_value=0, max_value=5000, value=1000)
+
+# Transformación de datos
+datos_prediccion = {
+    'areaAn': AREA_MAP.get(areaAn),
+    'sexo': SEXO_MAP.get(sexo),
+    'edadHTs': edadHTs,
+    'edadventa': edadventa
+}
+
+
+input_data = [[datos_prediccion['areaAn'], datos_prediccion['sexo'], 
+                datos_prediccion['edadHTs'], datos_prediccion['edadventa']]]
+
+# Botón para realizar todas las predicciones
+if st.button('Realizar todas las predicciones'):
+    # Realizar predicciones
+    predicciones = predict_all(input_data)
+
+else:
+    st.info('Ingrese los datos y haga clic en el botón para realizar las predicciones.')
+    
+    
+#Datos para guardar en la base de datos
+datos_supabase = {
+    'nombre': nombre_user,
+    'cargo': cargo_user,
+    'areaGranja': areaAn,
+    'sexo': sexo,   
+    'EdadSacrificio': edadHTs,
+    'EdadVenta': edadventa
+}
+    
+if st.button('Guardar predicciones'):
+    # Guardar las predicciones en la base de datos
+    st.subheader('Guardando predicciones...')
+    st.success('Predicciones guardadas exitosamente.')
+# Mostrar las predicciones
---
+++ utils/predicciones.py
@@ -0,0 +1,45 @@
+import joblib
+import streamlit as st
+import pandas as pd
+from xgboost import XGBRegressor
+
+# Cargar modelos
+model_porcMort = joblib.load('modelosPkl/model_porcMort.pkl')
+model_porcConsumo = joblib.load('modelosPkl/model_porcConsumo.pkl')
+model_ica = joblib.load('modelosPkl/model_ica.pkl')
+model_pesoProm = joblib.load('modelosPkl/model_pesoProm.pkl')
+
+# Funciones de predicción
+def predict_porcMort(X):
+    return model_porcMort.predict(X)
+
+def predict_porcConsumo(X):
+    return model_porcConsumo.predict(X)
+
+def predict_ica(X):
+    return model_ica.predict(X)
+
+def predict_pesoProm(X):
+    return model_pesoProm.predict(X)
+
+def predict_all(X):
+    porcMort = predict_porcMort(X)
+    porcConsumo = predict_porcConsumo(X)
+    ica = predict_ica(X)
+    pesoProm = predict_pesoProm(X)
+    # Realizar predicciones
+    resultados = {
+        'Porcentaje de Mortalidad': predict_porcMort(X)[0],
+        'Porcentaje de Consumo': predict_porcConsumo(X)[0],
+        'ICA': predict_ica(X)[0],
+        'Peso Promedio Final': predict_pesoProm(X)[0]
+    }
+    
+    # Crear DataFrame con los resultados
+    df_resultados = pd.DataFrame.from_dict(resultados, orient='index', columns=['Valor'])
+    
+    # Mostrar resultados
+    st.subheader('Resultados de las Predicciones')
+    st.dataframe(df_resultados.style.format({'Valor': '{:.2f}'}), use_container_width=True)
+    st.success('Predicciones realizadas con éxito!')
+    return df_resultados
