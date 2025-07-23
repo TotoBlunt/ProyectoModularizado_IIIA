857ea24996f5540e316dc834cd85ae6935685ac6
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2025-07-22 21:37:51 -0500

    update

--- app.py
+++ app.py
@@ -221,9 +221,10 @@ if archivo is not None:
                 df_resultado = df_subido[columnas_necesarias].copy()
                 df_resultado['Nombre Usuario'] = nombre_user if nombre_user else "No especificado"
                 df_resultado['Cargo Usuario'] = cargo_user if cargo_user else "No especificado"
-                df_resultado['Galpon'] = df_subido['Galpon']
+                
                 
                 # 4. Asignar las columnas directamente desde el DataFrame de resultados
+                df_resultado['Galpon'] = df_subido['Galpon']
                 df_resultado['prePorcMort'] = resultados_df['prePorcMort']
                 df_resultado['prePorcCon'] = resultados_df['prePorcCon']
                 df_resultado['preICA'] = resultados_df['preICA']
